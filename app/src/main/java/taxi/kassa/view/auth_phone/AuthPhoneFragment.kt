ec636052bc17d98d133c92a509abb7ee898ad45a
GeorgCantor <cupsman1986@gmail.com>
2020-02-05 19:20:30 +0300

    removed unused Java classes

--- app/src/main/java/taxi/kassa/view/auth_phone/AuthPhoneFragment.kt
+++ app/src/main/java/taxi/kassa/view/auth_phone/AuthPhoneFragment.kt
@@ -1,27 +1,21 @@
-package taxi.kassa.view.intro
+package taxi.kassa.view.auth_phone
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
-import kotlinx.android.synthetic.main.fragment_intro.*
 import taxi.kassa.R
 
-class IntroFragment : Fragment() {
+class AuthPhoneFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_intro, container, false)
+    ): View? = inflater.inflate(R.layout.fragment_auth_phone, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        btn_login.setOnClickListener {
-            Navigation.findNavController(it).navigate(R.id.authPhoneFragment)
-        }
     }
 }
