047826e099176fc8b93e580a9ce293ca7dc1de6b
GeorgCantor <cupsman1986@gmail.com>
2020-02-19 18:57:31 +0300

    fixed responses classes

--- app/src/main/java/taxi/kassa/model/responses/AccountsList.java
+++ app/src/main/java/taxi/kassa/model/responses/AccountsList.java
@@ -1,13 +1,19 @@
 package taxi.kassa.model.responses;
 
 import com.google.gson.annotations.SerializedName;
+
 import java.util.List;
 
 public class AccountsList {
-    // vars
+
     @SerializedName("info")
     private List<Account> info = null;
-    // methods
-    public List<Account> getInfo() {return info;}
-    public int getCount() {return info.size();}
+
+    public List<Account> getInfo() {
+        return info;
+    }
+
+    public int getCount() {
+        return info.size();
+    }
 }
--- app/src/main/java/taxi/kassa/model/responses/ResponseError.java
+++ app/src/main/java/taxi/kassa/model/responses/ResponseError.java
@@ -1,15 +1,21 @@
 package taxi.kassa.model.responses;
 
 import com.google.gson.annotations.SerializedName;
+
 import java.util.HashMap;
 
 public class ResponseError {
-    // vars
+
     @SerializedName("error_msg")
     private String error_msg = null;
     @SerializedName("error_data")
     private HashMap<String, String> error_data = null;
-    // methods
-    String getErrorMsg(){return error_msg;}
-    HashMap<String, String> getErrorData() { return error_data; }
+
+    String getErrorMsg() {
+        return error_msg;
+    }
+
+    HashMap<String, String> getErrorData() {
+        return error_data;
+    }
 }
