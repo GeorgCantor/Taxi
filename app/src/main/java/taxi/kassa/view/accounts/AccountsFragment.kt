5a981ed7814d8cec872b86dfd9a7f8c3f15da34c
GeorgCantor <cupsman1986@gmail.com>
2020-02-21 20:03:38 +0300

    added Accounts dialogs

--- app/src/main/java/taxi/kassa/view/accounts/AccountsFragment.kt
+++ app/src/main/java/taxi/kassa/view/accounts/AccountsFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_accounts.*
 import taxi.kassa.R
+import taxi.kassa.util.showDialog
 
 class AccountsFragment : Fragment() {
 
@@ -20,5 +21,19 @@ class AccountsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         back_arrow.setOnClickListener { activity?.onBackPressed() }
+
+        daily_withdrawal_tv.setOnClickListener {
+            context?.showDialog(
+                getString(R.string.daily_withdrawal),
+                getString(R.string.daily_withdrawal_dialog_message)
+            )
+        }
+
+        instant_withdrawal_tv.setOnClickListener {
+            context?.showDialog(
+                getString(R.string.instant_withdrawal),
+                getString(R.string.instant_withdrawal_dialog_message)
+            )
+        }
     }
 }
