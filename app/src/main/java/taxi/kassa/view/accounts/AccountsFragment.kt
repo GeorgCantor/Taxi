c44655c599bad0e399357f2f295c17a3c5ea44cb
GeorgCantor <cupsman1986@gmail.com>
2020-02-22 16:22:32 +0300

    added opening new account block

--- app/src/main/java/taxi/kassa/view/accounts/AccountsFragment.kt
+++ app/src/main/java/taxi/kassa/view/accounts/AccountsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.fragment_accounts.*
@@ -32,6 +33,8 @@ class AccountsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         viewModel.getAccounts()
 
+        val constraintSet = ConstraintSet()
+
         viewModel.error.observe(viewLifecycleOwner, Observer {
             activity?.shortToast(it)
         })
@@ -39,12 +42,14 @@ class AccountsFragment : Fragment() {
         viewModel.accounts.observe(viewLifecycleOwner, Observer {
             if (it.info.isNotEmpty()) {
                 account_block.visibility = View.VISIBLE
+                no_account_block.visibility = View.INVISIBLE
                 val account = it.info.first()
                 bank_name_tv.text = account?.bankName
                 order_tv.text = getString(R.string.order_format, account?.accountNumber)
                 name_tv.text = account?.driverName
             } else {
                 account_block.visibility = View.INVISIBLE
+                no_account_block.visibility = View.VISIBLE
             }
         })
 
@@ -62,6 +67,36 @@ class AccountsFragment : Fragment() {
                 getString(R.string.instant_withdrawal),
                 getString(R.string.instant_withdrawal_dialog_message)
             )
+        }
+
+        add_account_image.setOnClickListener {
+            no_account_block.visibility = View.INVISIBLE
+            account_block.visibility = View.INVISIBLE
+            new_account_block.visibility = View.VISIBLE
+
+            constraintSet.clone(parent_layout)
+            constraintSet.connect(
+                R.id.instant_withdrawal_tv,
+                ConstraintSet.TOP,
+                R.id.new_account_block,
+                ConstraintSet.BOTTOM
+            )
+            constraintSet.applyTo(parent_layout)
+        }
+
+        close_image.setOnClickListener {
+            no_account_block.visibility = View.VISIBLE
+            account_block.visibility = View.INVISIBLE
+            new_account_block.visibility = View.INVISIBLE
+
+            constraintSet.clone(parent_layout)
+            constraintSet.connect(
+                R.id.instant_withdrawal_tv,
+                ConstraintSet.TOP,
+                R.id.account_block,
+                ConstraintSet.BOTTOM
+            )
+            constraintSet.applyTo(parent_layout)
         }
     }
 }
