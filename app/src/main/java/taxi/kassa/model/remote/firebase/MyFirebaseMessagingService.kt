19aca8c948beb26b5825d6ecc287018b2bc8aa9d
GeorgCantor <cupsman1986@gmail.com>
2020-03-17 14:44:44 +0300

    added Firebase messaging

---
+++ app/src/main/java/taxi/kassa/model/remote/firebase/MyFirebaseMessagingService.kt
@@ -0,0 +1,17 @@
+package taxi.kassa.model.remote.firebase
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onNewToken(s: String) {
+        super.onNewToken(s)
+        Log.e("NEW_TOKEN", s)
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+    }
+}
