da50bdabcaa21520c758316b03b47a4df52f0464
GeorgCantor <cupsman1986@gmail.com>
2020-05-02 13:49:24 +0300

    try to fix gitignore

---
+++ app/build/generated/source/buildConfig/debug/taxi/kassa/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package taxi.kassa;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "taxi.kassa";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 21;
+  public static final String VERSION_NAME = "1.21";
+  // Fields from default config.
+  public static final String BASE_URL = "https://kassa.taxi/api/";
+}
